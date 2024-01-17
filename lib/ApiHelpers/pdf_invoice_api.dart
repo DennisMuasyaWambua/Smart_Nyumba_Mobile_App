283a9b9e8df5dddc8edfda3a020a3d6e344f0780
Victor Mutuku <victor.mutuku@strathmore.edu>
2024-01-17 16:44:53 +0300

    added permission_handler dependency

--- lib/ApiHelpers/pdf_invoice_api.dart
+++ lib/ApiHelpers/pdf_invoice_api.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-// import 'package:permission_handler/permission_handler.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart';

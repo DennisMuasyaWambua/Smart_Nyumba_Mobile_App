a5f1e5ce7a2c7f3b328e8da1517f24f6c3f3defd
Victor Mutuku <victor.mutuku@strathmore.edu>
2024-02-03 08:55:02 +0300

    Completed companies card

--- lib/widgets/admin/companies_summary_card.dart
+++ lib/widgets/admin/companies_summary_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../screens/admin/companies.dart';
+import '../../utils/constants/colors.dart';
+import '../status_indicator.dart';
 
 class CompaniesSummaryCard extends StatelessWidget {
   const CompaniesSummaryCard({Key? key}) : super(key: key);
@@ -9,8 +11,12 @@ class CompaniesSummaryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Navigator.of(context).pushNamed(Companies.routeName),
+      onTap: () => Navigator.of(context).pushNamed(
+        Companies.routeName,
+        arguments: [""],
+      ),
       child: Card(
+        elevation: 5,
         child: Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: 20,
@@ -26,38 +32,110 @@ class CompaniesSummaryCard extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-             const SizedBox(
+              SizedBox(
                 height: 150,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Text(
-                            "2",
-                            style:  TextStyle(fontSize: 32),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () => Navigator.of(context).pushNamed(
+                            Companies.routeName,
+                            arguments: ['active'],
                           ),
-                           SizedBox(height: 10),
-                          Text("Active"),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Text(
-                            "2",
-                            style: TextStyle(
-                              fontSize: 32,
-                              color: Colors.grey, // Assuming lightGrey is a Color variable
+                          style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                              (Set<MaterialState> states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return Colors.transparent;
+                                }
+                                return null;
+                              },
                             ),
                           ),
-                           SizedBox(height: 10),
-                          Text("Past"),
-                        ],
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "2",
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    "Active",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  StatusIndicator(color: darkGreen),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () => Navigator.of(context).pushNamed(
+                            Companies.routeName,
+                            arguments: ['past'],
+                          ),
+                          style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                              (Set<MaterialState> states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return Colors.transparent;
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "2",
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  color: Colors.grey, // Assuming lightGrey is a Color variable
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    "Past",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  StatusIndicator(color: lightGrey),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
